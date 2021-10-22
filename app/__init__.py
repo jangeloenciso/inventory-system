53d78c4457725220e4c5fd2f7986d4ee139e7ea7
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2021-10-22 15:07:47 +0800

    integrated with postgres

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, migrate
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/inventory-system'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
