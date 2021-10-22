17081071a1f7d7560d989c2287ef81ef1013d233
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2021-10-22 22:42:00 +0800

    routing and initial post request attempt

--- app/forms.py
+++ app/forms.py
@@ -6,9 +6,10 @@ from wtforms.fields.core import FloatField, IntegerField
 from wtforms.validators import DataRequired
 from app.models import Product
 
-class ProductUpdateForm(FlaskForm):
+class ProductForm(FlaskForm):
     product_name = StringField('Product Name', validators=[DataRequired()])
     product_desc = TextAreaField('Product Description')
     qty_stock = IntegerField("Stock Quantity")
     price = FloatField("Price")
     on_hand = IntegerField("On hand quantity")
+    submit = SubmitField('Add')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
-from app import app
+from app import app, db
+from flask import render_template, redirect
+from app.models import Product
+from app.forms import ProductForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    form = ProductForm()
+    if form.validate_on_submit():
+        product = Product(
+            product_name = form.product_name.data,
+            product_desc = form.product_desc.data,
+            qty_stock = form.qty_stock,
+            price = form.price.data,
+            on_hand = form.on_hand
+        )
+        db.session.add(product)
+        db.session.commit()
+    products = Product.query.all()
+    return render_template('index.html', form=form, products=products)
